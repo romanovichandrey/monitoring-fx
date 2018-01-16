12baf2bc96deeefe5dd08ac8c69a3baf14023704
Romanovich_AG <romanovichandrey1989@gmail.com>
2018-01-16 09:39:37 +0300

    *complite context

--- monitoring-fx-ui/src/main/java/by/bytechs/ui/config/MonitoringApp.java
+++ monitoring-fx-ui/src/main/java/by/bytechs/ui/config/MonitoringApp.java
@@ -2,14 +2,9 @@ package by.bytechs.ui.config;
 
 import by.bytechs.restService.config.RestServiceConfig;
 import by.bytechs.service.config.ServiceConfig;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
+import by.bytechs.ui.view.HelloWordOverview;
+import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -17,30 +12,8 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication(scanBasePackages = "by.bytechs.ui")
 @Import({RestServiceConfig.class, ServiceConfig.class})
-public class MonitoringApp extends Application {
-    private ConfigurableApplicationContext springContext;
-    private Parent rootNode;
-
+public class MonitoringApp extends AbstractJavaFxApplicationSupport {
     public static void main(String[] args) {
-        Application.launch(args);
-    }
-
-    @Override
-    public void init() throws Exception {
-        springContext = SpringApplication.run(new Class<?>[]{MonitoringApp.class, RestServiceConfig.class, ServiceConfig.class});
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/HelloWordFxml.fxml"));
-        fxmlLoader.setControllerFactory(springContext::getBean);
-        rootNode = fxmlLoader.load();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        springContext.close();
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(rootNode));
-        primaryStage.show();
+        launch(MonitoringApp.class, HelloWordOverview.class, args);
     }
 }
--- monitoring-fx-ui/src/main/java/by/bytechs/ui/controller/HelloWordController.java
+++ monitoring-fx-ui/src/main/java/by/bytechs/ui/controller/HelloWordController.java
@@ -1,19 +1,23 @@
 package by.bytechs.ui.controller;
 
+import by.bytechs.service.HelloWordService;
+import de.felixroske.jfxsupport.FXMLController;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Romanovich Andrei
  */
-@Component
+@FXMLController
 public class HelloWordController {
     @FXML
     TextField helloWordTextField;
+    @Autowired
+    private HelloWordService helloWordService;
 
     @FXML
     public void initialize() {
-
+        helloWordTextField.setText(helloWordService.getHelloWord());
     }
 }
